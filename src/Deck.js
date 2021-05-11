cdd235dea449e9666eccdf2279384fbc7069c778
Jim Charnesky <mijnesky@hotmail.com>
2021-05-11 16:43:48 -0600

    Set up describe block, for each statment, and test for a Deck class

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,9 @@
+// should be initialized (parameter>>) with an array of Card objects
+// should know (return) how many cards are in the deck
+
 class Deck {
   constructor() {
-    
+
   }
 }
 
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,1 +1,22 @@
 // deck
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck.js');
+const Card = require('../src/Card.js');
+
+describe('Deck', () => {
+  let card1, card2, card3, deck;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'What is the color of the sky?', ['blue', 'yellow', 'green'], 'blue');
+    card2 = new Card(2, 'What is the color of sand?', ['blue', 'green', 'brown'], 'brown');
+    card3 = new Card(3, 'What is the color of aluminium foil?', ['pink', 'silver', 'brown'], 'silver');
+    deck = new Deck(card);
+  });
+
+  it('should be able to hold an array of cards', () => {
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+});
