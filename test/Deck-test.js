2565a226ebf494d5ce6275764d5e1833e8d00663
Jim Charnesky <mijnesky@hotmail.com>
2021-05-11 19:15:51 -0600

    Add tests to build deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -13,10 +13,18 @@ describe('Deck', () => {
     card1 = new Card(1, 'What is the color of the sky?', ['blue', 'yellow', 'green'], 'blue');
     card2 = new Card(2, 'What is the color of sand?', ['blue', 'green', 'brown'], 'brown');
     card3 = new Card(3, 'What is the color of aluminium foil?', ['pink', 'silver', 'brown'], 'silver');
-    deck = new Deck(card);
+    deck = new Deck([card1, card2, card3]);
+  });
+
+  it('should be a deck', () => {
+    expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should be able to hold an array of cards', () => {
-    expect(deck).to.be.an.instanceof(Deck);
+    expect(deck.cards).to.equal([card1, card2, card3]);
+  });
+
+  it('should be able to return how many cards are in the deck', () => {
+    expect(deck.countCards()).to.deep.equal(3);
   });
 });
