9312d3726ba2f40f9a2dd9364cbadfcecef0338d
Jim Charnesky <mijnesky@hotmail.com>
2021-05-11 20:40:26 -0600

    Add tests to build properties of Round class

--- src/Round.js
+++ src/Round.js
@@ -1,7 +1,18 @@
 class Round {
   constructor() {
-    
+    // pass through and store deck
+    // takes in responses
+    // records if guess is incorrect
+    // keep track of turn count
+    // store current turn
   }
+  // currentCard should be first in deck at start of round
+  // returnCurrentCard returns the current card being played
+  // takeTurn: update turn count; use Turn class to evaluate guesses, give feedback; store incorrect guess ids
+  // calculatePercentCorrect: calculate and return percent correct
+  // endRound: prints  ‘** Round over! ** You answered <>% of the questions correctly!’
+
+
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -7,5 +7,33 @@ const Card = ('../src/Card');
 const Turn = ('../src/Round');
 
 describe('Round', () => {
-  let 
+  let card1, card2, card3, deck, round;
+
+  beforeEach(() => {
+    card1 = new Card(1, 'What is the color of the sky?', ['blue', 'yellow', 'green'], 'blue');
+    card2 = new Card(2, 'What is the color of sand?', ['blue', 'green', 'brown'], 'brown');
+    card3 = new Card(3, 'What is the color of aluminium foil?', ['pink', 'silver', 'brown'], 'silver');
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+  });
+
+  it('should be a round', () => {
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it.skip('should be able to take in a deck', () => {
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
+  });
+
+  it.skip('should start off with turns set at zero', () => {
+    expect(round.turns).to.equal(0);
+  });
+
+  it.skip('should be able to store incorrect guesses', () => {
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it.skip('should start off with current turn set to null', () => {
+    expect(round.currentTurn).to.equal(null);
+  })
 });
