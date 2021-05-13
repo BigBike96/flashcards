d29b1a6afc72fc27982c1ee7ce31ff820e3b1478
Jim Charnesky <mijnesky@hotmail.com>
2021-05-12 21:11:26 -0600

    Skip tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -12,13 +12,13 @@ describe('Game', () => {
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it.skip('should have a way to keep track of current round', () => {
+  it('should have a way to keep track of current round', () => {
     const game = new Game();
 
     expect(game.currentRound).to.equal(null);
   });
 
-  it.skip('should have a new instance of round after the game has started', () => {
+  it('should have a new instance of round after the game has started', () => {
     const game = new Game();
 
     game.start();
