26ec411634d18fae7cfe1159e62e1372a18d767e
Jim Charnesky <mijnesky@hotmail.com>
2021-05-12 20:04:43 -0600

    Finish methods with tdd

--- src/Round.js
+++ src/Round.js
@@ -1,24 +1,35 @@
 const Turn = require('../src/Turn');
-const Card = require('../src/Card');
 
 class Round {
   constructor(deck) {
     this.deck = deck;
-    // pass through and store deck
     this.turns = 0;
-    // keep track of turn count
     this.incorrectGuesses = [];
-    // records if guess is incorrect
     this.currentTurn = null;
   }
 
   returnCurrentCard() {
     return this.deck.cards[this.turns];
   }
-  // returnCurrentCard returns the current card being played
-  // takeTurn: update turn count; use Turn class to evaluate guesses, give feedback; store incorrect guess ids
-  // calculatePercentCorrect: calculate and return percent correct
-  // endRound: prints  ‘** Round over! ** You answered <>% of the questions correctly!’
+
+  takeTurn(guess) {
+    this.currentTurn = new Turn(guess, this.returnCurrentCard());
+    if (!this.currentTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.returnCurrentCard().id);
+    }
+    this.turns++;
+    return this.currentTurn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    return this.incorrectGuesses.length / this.turns;
+  }
+
+  endRound() {
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
+    process.exit();
+  }
 }
+
 
 module.exports = Round;
