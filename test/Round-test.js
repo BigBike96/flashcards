5dd536668d1cce5382652d5a14d5601c96ab4b76
Jim Charnesky <mijnesky@hotmail.com>
2021-05-12 20:05:01 -0600

    Fix typos

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,30 +3,34 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
-const Round = require('../src/Round');
-const Turn = require('../src/Round');
 const Card = require('../src/Card');
+const Round = require('../src/Round');
+const Turn = require('../src/Turn');
 
 describe('Round', () => {
   let card1, card2, card3, deck, round;
 
   beforeEach(() => {
-    card1 = new Card(1, 'What is the color of the sky?', ['blue', 'yellow', 'green'], 'blue');
-    card2 = new Card(2, 'What is the color of sand?', ['blue', 'green', 'brown'], 'brown');
-    card3 = new Card(3, 'What is the color of aluminium foil?', ['pink', 'silver', 'brown'], 'silver');
+    card1 = new Card(1, 'What is the color of the night sky?', ['yellow', 'black', 'green'], 'black');
+    card2 = new Card(2, 'How many days in a month?', ['3', '9', '30'], '30');
+    card3 = new Card(3, 'What is the capitol of Wyoming?',['Cheyanne', 'Laramie', 'Lander'], 'Laramie');
     deck = new Deck([card1, card2, card3]);
     round = new Round(deck);
   });
 
-  it('should be a round', () => {
+  it('should be an instance of a Round', () => {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it('should be able to take in a deck', () => {
+  it('should take in a Deck', () => {
     expect(round.deck).to.deep.equal(deck);
   });
 
-  it('should start off with turns set at zero', () => {
+  it('should be able to return the current Card', () => {
+    expect(round.returnCurrentCard()).to.equal(card1);
+  });
+
+  it('should start out with no turns', () => {
     expect(round.turns).to.equal(0);
   });
 
@@ -34,15 +38,7 @@ describe('Round', () => {
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
 
-  it('should start off with current turn set to null', () => {
-    expect(round.currentTurn).to.equal(null);
-  });
-
-  it('should be able to return the current card', () => {
-    expect(round.returnCurrentCard()).to.equal(card1);
-  });
-
-  it.skip('should add a turn to the turn count', () => {
+  it('should increase the turn count by one with each turn', () => {
     expect(round.turns).to.equal(0);
 
     round.takeTurn();
@@ -54,40 +50,40 @@ describe('Round', () => {
     expect(round.turns).to.equal(2);
   });
 
-  it.skip('should add a turn when a guess is made', () => {
+  it('when a guess is made, a new turn should be created', () => {
     const guess = 'yellow';
     round.takeTurn(guess);
 
     expect(round.currentTurn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should make the next card a current card when a turn is taken', () => {
-    const turn1 = round.takeTurn('red');
-
+  it('should make the next card a current card when a turn is taken', () => {
+    round.takeTurn();
 
     expect(round.returnCurrentCard()).to.equal(card2);
   });
 
-  it.skip('should check the guess and give correct feedback response', () => {
-    round.takeTurn('blue');
-    // this will need to be adjusted
+  it('should evaluate the guess and return appropiate feedback', () => {
+    const turn1 = round.takeTurn('black');
+
     expect(turn1).to.equal('correct!');
+    expect(round.returnCurrentCard()).to.equal(card2);
   });
 
-  it.skip('should store id of incorrect guess', () => {
+  it('should store id of incorrect guess', () => {
     const turn2 = round.takeTurn('yellow');
 
     expect(round.incorrectGuesses).to.deep.equal([1]);
   });
 
-  it.skip('should calculate and return percent of correct guesses', () => {
+  it('should calculate and return percent of correct guesses', () => {
     round.takeTurn('black');
-    round.takeTurn('brown');
+    round.takeTurn('9');
 
     expect(round.calculatePercentCorrect()).to.equal(.50);
   });
 
-  it.skip('should return statment to user to indicate the round is over', () => {
+  it('should return statment to user to indicate the round is over', () => {
     round.takeTurn('black');
     round.endRound();
 
